343b0c5292694a8809bd831626e3d8685326b7aa
Bestian Tang <bestian@gmail.com>
2021-06-27 14:54:29 +0800

    v1

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -12,10 +12,10 @@
         />
 
         <q-toolbar-title>
-          Quasar App
+          易經占卜
         </q-toolbar-title>
 
-        <div>Quasar v{{ $q.version }}</div>
+        <div>第1版</div>
       </q-toolbar>
     </q-header>
 
@@ -52,46 +52,10 @@ import EssentialLink from 'components/EssentialLink.vue'
 
 const linksList = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
+    title: '周易',
+    caption: 'https://zh.wikisource.org/wiki/周易',
     icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
+    link: 'https://zh.wikisource.org/wiki/%E5%91%A8%E6%98%93'
   }
 ];
 
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,11 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="container">
+    <div v-if ="start">
+      <b>你今天的卦是: {{names[n]}}{{list[n]}}</b>
+      <iframe width="100%" :src="'https://zh.wikisource.org/wiki/%E5%91%A8%E6%98%93/' + encodeURI(names[n])"></iframe>
+      <button @click="reset()">再來一次</button>
+    </div>
+    <button v-else class="big" @click="go()">按此開始</button>
   </q-page>
 </template>
 
@@ -12,6 +13,41 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'PageIndex'
+  name: 'PageIndex',
+  data() {
+    return {
+      start: false,
+      n: 0,
+      list: ['䷀', '䷁', '䷂', '䷃', '䷄', '䷅', '䷆', '䷇', '䷈', '䷉', '䷊', '䷋', '䷌', '䷍', '䷎', '䷏', '䷐', '䷑', '䷒', '䷓', '䷔', '䷕', '䷖', '䷗', '䷘', '䷙', '䷚', '䷛', '䷜', '䷝', '䷞', '䷟', '䷠', '䷢', '䷣', '䷤', '䷥', '䷦', '䷧', '䷨', '䷩', '䷪', '䷫', '䷬', '䷭', '䷮', '䷯', '䷰', '䷱', '䷲', '䷳', '䷴', '䷵', '䷶', '䷷', '䷸', '䷹', '䷺', '䷻', '䷼', '䷽', '䷾', '䷿'],
+      names: ['乾', '坤', '屯', '蒙', '需', '訟', '師', '比', '小畜', '履', '泰', '否', '同人', '大有', '謙', '豫', '隨', '蠱', '臨', '觀', '噬嗑', '賁', '剝', '復', '无妄', '大畜', '頤', '大過', '坎', '離', '咸', '恆', '遯', '大壯', '晉', '明夷', '家人', '睽', '蹇', '解', '損', '益', '夬', '姤', '萃', '升', '困', '井', '革', '鼎', '震', '繼', '艮', '漸', '歸妹', '豐', '旅', '巽', '兌', '渙', '節', '中孚', '小過', '既濟', '未濟']
+    }
+  },
+  methods: {
+    go() {
+      this.start = true
+      this.reset()
+    },
+    reset() {
+      this.n = Math.floor(Math.random()*64);
+    }
+  },
+  mounted() {
+    this.reset()
+  }
 })
 </script>
+
+<style type="text/css" scoped="">
+
+  b {
+    font-size: 24px;
+  }
+  
+  iframe {
+    height: 75vh;
+  }
+
+  .big {
+    font-size: 66px;
+  }
+</style>
